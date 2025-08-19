4bcdffbb0bded645fe1d07112a54170cd32458f8
dhruvburada <buradadhruv35@gmail.com>
2025-08-19 10:09:16 +0530

    start test 10:09

---
+++ src/app/inputbox/inputbox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inputbox',
+  imports: [],
+  templateUrl: './inputbox.component.html',
+  styleUrl: './inputbox.component.scss'
+})
+export class InputboxComponent {
+
+}
